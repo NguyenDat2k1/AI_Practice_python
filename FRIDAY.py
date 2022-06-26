50f02e7fcea84e6ae8fb7dce99012f24ad7b287f
Nguyễn Tiến Đạt <646626@sv.vnua.edu.vn>
2022-06-26 19:36:09 +0700

    push friday

---
+++ FRIDAY.py
@@ -0,0 +1,61 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import webbrowser as wb
+import os
+friday=pyttsx3.init()
+voice=friday.getProperty('voices')
+friday.setProperty('voice',voice[1].id)
+
+def speak(audio):
+    print('F.R.I.D.A.Y. '+ audio)
+    friday.say(audio)
+    friday.runAndWait()
+def time():
+    Time=datetime.datetime.now().strftime("%I:%M:%p") 
+    speak(Time)
+def wellcome(): 
+    hour = datetime.datetime.now().hour
+    if hour >=6 and hour <=12 :
+        speak("Good morning Sir")   
+    if hour >=13 and hour <=18 :
+        speak("Good Afternoon Sir")       
+    if hour >=19 and hour <=24 :
+        speak("Good Evening Sir")   
+    speak('How can i help you')
+def command():
+    c=sr.Recognizer() 
+    with sr.Microphone() as source:
+        c.pause_threshold=2
+        audio=c.listen(source)
+    try:
+        query=c.recognize_google(audio,language='en')
+        print("Dat2k1 :" + query)
+    except sr.UnknownValueError:
+        print("please repeat or typing the command")
+        query=str(input('Your order is:'))
+    return query       
+
+
+
+if __name__ =="__main__":
+    wellcome()  
+    while True :
+        query=command().lower()
+        if"google" in query:
+            speak("what should i search boss")
+            search=command().lower()
+            url=f"http://www.google.com/search?q={search}"
+            wb.get().open(url)
+            speak(f'here is your {search} on google')
+        if"youtube" in query:
+            speak("what should i search boss")
+            search=command().lower()
+            url=f"http://www.youtube.com/search?q={search}"
+            wb.get().open(url)
+            speak(f'here is your {search} on youtube')
+        elif "time" in query:
+            time() 
+        elif "quit" in query:
+            speak("good bye sir,i turning off")      
+            quit()    
